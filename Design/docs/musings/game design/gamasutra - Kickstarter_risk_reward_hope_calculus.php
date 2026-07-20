5e69336e1510152cf74ebb653770499eedc0c0c9
Michael Preston Joseph <michaelwalkswithgod@gmail.com>
2026-07-20 16:11:01 -0700

    musings part 3

---
+++ Design/docs/musings/game design/gamasutra - Kickstarter_risk_reward_hope_calculus.php
@@ -0,0 +1,14 @@
+feb.25.2014 - kickstarter investment calculus
+
+ RE: A Look at Kickstarted Video Game Delivery Rates
+by Un Subject on 02/25/14 12:55:00 pm
+http://www.gamasutra.com/blogs/UnSubject/20140225/210645/A_Look_at_Kickstarted_Video_Game_Delivery_Rates.php
+--
+
+"I’m not willing to spend money and accept the risk of a project failing."
+
+Well whatever floats your boat, but I think for some projects and some customers their is a risk that a project they really want made will not get funded and not get made. This makes it not just a "trust" exercise but a "hope" exercise. And there's a certain feel good factor involved with "having the vision" to back a certain project and then being one of the people who directly helped to make it happen.
+
+All I'm saying is there's a specific type of risk and reward that you're ignoring in your investment calculus but which backers do not.
+
+Also if you ever plan on launching a ks campaign yourself, being able to show a history of ks support may help. 
